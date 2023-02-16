63899b5d49958137d21a526c888ffcadd5d88a38
calvinucheoma <calvinchukwuma@gmail.com>
2023-02-16 23:13:36 +0100

    initial commit

--- app.js
+++ app.js
@@ -7,14 +7,20 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
 
 app.get('/', (req, res) => {
-  res.send('jobs api');
+  res.send('<h1>Jobs API </h1> <a href="/api-docs">Documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 //routers
 const authRouter = require('./routes/auth');
